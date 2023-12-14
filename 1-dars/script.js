cfe8ad22b8bd5a7f776a78395d2aeb051a7d4391
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-14 12:54:27 -0800

    variables, data types, js how its work, thread, engine, declare variable

--- 1-dars/script.js
+++ 1-dars/script.js
@@ -1,1 +1,96 @@
-        console.log("Hello World")
+
+// console.log(a);
+// let a = 1;
+
+// const a = "Home";
+// a = 1;
+
+// const pi = 3.14
+// console.log(a);
+
+
+
+// console.log(a); // ERROR - a is not defined
+// a = 1;
+
+
+// console.log(a); // WARNING - UNDEFINED
+// var a = 1;
+
+
+// console.log(a); // REFERENCE ERROR - CANNOT ACCESS a "before" initilaziation
+// const a = 1;
+
+
+// console.log(a); // REFERENCE ERROR - CANNOT ACCESS a "before" initilaziation 
+// let a = 1;
+
+
+// THREAD - SINGLE THREAD - MULTIPLE THREAD
+
+// VARIABLE larni nomlaguncha ortiqcha symbollardan foydalanmaslik kerak va reserverd keywordlardan ham
+// reserverd keywords (function,var,const,let,{}, string,number,boolean,undefined,null,bigInt)
+// let a = 1;
+
+
+// DATA TYPES
+// 8 ta
+
+// PRIMITIVE
+  // STRING
+  // BOOLEAN
+  // UNDEFINED
+  // BIGINT
+  // NUMBER
+  // SYMBOL
+
+// REFERENCE - NON PRIMITIVE
+  // OBJECT
+  // NULL
+
+
+//  STRING DATA TYPE
+// let text = String("");
+// let text2 = '';
+// let text3 = ``;
+// console.log(typeof text);
+// console.log(typeof text2);
+// console.log(typeof text3);
+
+
+// BOOLEAN DATA TYPE
+// let result = Boolean(true);
+// let result = false;
+// console.log(typeof result);
+
+
+// UNDEFINED DATA TYPE
+// let a;
+// console.log(typeof a);
+
+// BIGINT DATA TYPE
+// let bigNum = BigInt(2);
+// console.log(typeof bigNum);
+
+// NUMBER DATA TYPE
+// let num = Number(2);
+// let num = 2;
+// console.log(typeof num);
+
+// SYMBOL DATA TYPE
+// let sym = Symbol("id");
+// console.log(typeof sym);
+
+
+// OBJECT DATA TYPE
+// let obj = {
+//        firsName:"John",
+//        age: 18,
+//        isMarried:true,
+// }
+// console.log(obj);
+
+// NULL DATA TYPE
+// let empty = null;
+// console.log(typeof empty);
+
