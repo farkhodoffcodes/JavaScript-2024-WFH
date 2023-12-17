f1a5c6804148d781085d26227f70b2816b73ec87
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-17 10:20:11 -0800

    2-dars FizzBuzz/ if else

---
+++ 3-dars/script.js
@@ -0,0 +1,178 @@
+// 3-DARS
+
+// console.log(null == undefined); // true
+// console.log(null === undefined); // false
+
+// console.log(1 == "1"); // true
+// console.log(1 === "1"); // false 
+
+// console.log("" == false); // true
+// console.log("" === false); // true
+
+
+// console.log(typeof ""); // string
+
+
+// console.log(0 == false); // true
+// console.log(true === true); // true
+// console.log(false === true); // false
+// console.log(0 === false); // false
+// console.log("0" === false); // false
+
+
+// let a = 1;
+// console.log(a++);
+// console.log(a);
+
+// console.log(++a);
+// console.log(++a);
+
+// let a = 1;
+
+// INCREMENT
+// console.log(++a);
+// console.log(a++);
+
+// DECREMENT
+
+// console.log(--a); // 0
+// console.log(a--); // 1
+// console.log(a); // 0
+
+
+
+// let obj = {
+//     name:"Logo",
+//     age:18,
+//     head: {
+//         hair: "curley",
+//         color: "brown"
+//     }
+// }
+
+
+// // console.log(obj.head.color);
+
+// console.log(obj.head.headSize = 12);
+// console.log(obj);
+
+
+// TYPE CONVERSION
+
+// let a = 4;
+// console.log((a + "2")); //42
+// console.log(typeof  (a + "2")); //string
+
+
+// TO BOOLEAN
+
+
+// let a = 0;
+// console.log(!a);
+
+
+// console.log(1 / 0); // INFINITY ?
+// console.log(-1 / 0); // -INFINITY ?
+
+
+
+
+// LOGICAL OPERATORS
+
+// && - AND
+// console.log(true && false); // false
+// console.log(false && false); // false
+// console.log(false && true); // false
+// console.log(true && true); // false
+
+// // || - OR
+// console.log(false || true); // true
+// console.log(false || false); // false
+// console.log(true || false ); // false
+// console.log(true || true ); // true
+
+
+// ! - NOT
+// console.log(!true); // false
+// console.log(!false); // true
+// console.log(!1); //  false
+// console.log(!0); //  true
+
+
+// console.log(!(true && false || false && "hey"));
+
+
+// console.log(!(1 && "1" || true && undefined));
+// console.log(1 || undefined);
+
+// console.log(undefined && null);
+// console.log(false && true);
+
+// console.log(2 || 1);
+
+
+// console.log(undefined || null || 0);
+
+
+// console.log("A" == "a");
+
+
+
+// SHART
+// let user = 14;
+
+// console.log(18);
+// let a = 15;
+
+
+// if(a >= 18) {
+//     console.log("Hush kelibsiz");
+// }else if(a == 14) {
+//     console.log("Siz Xali bor");
+// }else {
+//     console.log("Sizga mumkin emas");
+// }
+
+// FIZZBUZZ
+
+// SON 3 qoldiqsiz bo'linsa  == Fizz chiqsin
+// SON 5 qoldiqsiz bo'linsa  == Buzz chiqsin
+// SON 5 va 3 ga qoldiqsiz bo'linsa  == FizzBuzz chiqsin
+
+
+// let a =3 * 3 = 27;
+//let a = 4 * 2 = 8;
+
+
+// let num = 27;
+
+// if(num % 2 === 0) {
+//     console.log("Juft Son");
+// }else {
+//     console.log("Toq son");
+// }
+
+
+// let a = 10;
+
+// if(a % 3 === 0 && a % 5 === 0) {
+//     console.log("FizzBuzz");
+// }else if(a % 3 === 0 ) {
+//     console.log("Fizz");
+// }else if(a % 5 === 0) {
+//     console.log("Buzz");
+// }else {
+//     console.log("Something went wrong");
+// }
+
+
+// WRONG WAY 
+
+// if(a / 3){
+//   console.log('Fizz')
+// }else if(a / 5){
+//     console.log('Buzz')
+// }else{
+//     console.log('FizzBuzz')
+// }
+
