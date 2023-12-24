78c3caca7a016362cb4efb54d27df3be3f7d4968
farkhodoff19 <farkhodoffcodes@gmail.com>
2023-12-24 02:50:23 -0800

    8-dars / Bitwise Operator / String Methods / Object Methods

---
+++ 8-dars/script.js
@@ -0,0 +1,73 @@
+// console.log(f());
+
+// function f() {
+// }
+
+
+// for(let i = 0; i < 10;) {
+//     // no semicolon needed after the loop
+//     console.log(i);
+//   }
+
+// let s = () => {}
+// console.log( function(){} == s);
+
+
+// BITWISE AND &
+// console.log("trie" & true); // 0
+
+
+// BITWISE OR |
+// console.log(0 | true); // 1
+
+// console.log(!null & !undefined); // 1
+// console.log(!null & undefined); // 0
+// console.log((!null & undefined)) // 1
+
+// console.log(!!null);
+
+// console.log(-1 ^ 9);
+
+
+// let elH1 = document.querySelector("#heading")
+// let elH2 = document.querySelector(".heading")
+
+
+
+// METHOD
+
+// let obj = {
+//     name: "John",
+//     speak: function() {
+//         console.log("Hey")
+//     }
+// }
+
+// obj.speak();
+
+// console.log(Math.pow(2,4));
+
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.substring(7, 13);
+
+
+// console.log(part);
+
+
+// let text = "Please visit Microsoft! !Microsoft";
+// let newText = text.replace(/Microsoft/g, "W3Schools");
+
+
+// console.log(newText);
+
+// // TRIM
+// let text1 = "      Hello World!      ";
+// let text2 = text1.trim();
+
+// console.log(text2.length);
+
+
+// let a = 5;
+
+// console.log(typeof a.toString());
